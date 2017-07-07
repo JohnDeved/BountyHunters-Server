8387fd59ce7ceddc043a804f7edb9669bb08e01b
DevSec <arma.devsec@gmail.com>
2017-07-07 02:06:30 +0200

    + getplayeritems

--- addons/bountyhunters_server/config.cpp
+++ addons/bountyhunters_server/config.cpp
@@ -58,6 +58,7 @@ class CfgFunctions
             class init {};
             class findItem {};
             class getPlayerTotalWeigth {};
+            class getPlayerItems {};
         };
     };
 };
